04bfe783bb3586a8f843b513b124848db9f2e7bc
Kelly Tan <k.k.tan@outlook.com>
2024-08-07 21:41:55 +1000

    WEEK 3: ACTIVITIES 1-4 bootstrap installed

--- WEEK-2/ktan-library/src/main.js
+++ WEEK-2/ktan-library/src/main.js
@@ -4,3 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+//import './style.css'
+//will now be using bootstrap://
+
+import 'bootstrap/dist/css/bootstrap.min.css'
